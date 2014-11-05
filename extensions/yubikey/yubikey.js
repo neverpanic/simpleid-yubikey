c944f2deb35bba439441c9548b86cd7e3927a6a4
Clemens Lang <neverpanic@gmail.com>
2014-11-05 22:10:58 +0100

    Set focus to Yubikey field on load

--- extensions/yubikey/yubikey.js
+++ extensions/yubikey/yubikey.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('input#edit-name').val('yubikey');
-    $('input#edit-pass').addClass('yubiKeyInput');
+	$('input#edit-pass').addClass('yubiKeyInput');
 	$('input#edit-pass').siblings('label').text('Yubikey:');
+	$('input#edit-pass').focus();
 });
